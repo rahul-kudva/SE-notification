36bedf7405337c980c50b5bd8d8d94ac3de6ce7c
rahul-kudva <rahulkudva98@gmail.com.com>
2019-11-16 13:46:48 +0530

    First commit

---
+++ mail.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+from flask_mail import Mail, Message
+app = Flask(__name__)
+
+# initialize scheduler with your preferred timezone
+scheduler = BackgroundScheduler({'apscheduler.timezone': 'Asia/Calcutta'})
+scheduler.start()
+
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": "krk141298@gmail.com",
+    "MAIL_PASSWORD": "rahul12345"
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+
+@app.route('/schedulePrint', methods=['POST'])
+def schedule_to_print():
+    #get time to schedule and text to print from the json
+    time = "2019-11-13 21:50:00"
+    #convert to datetime
+    date_time = datetime.strptime(str(time), '%Y-%m-%d %H:%M:%S')
+    #schedule the method 'printing_something' to run the the given 'date_time' with the args 'text'
+    job = scheduler.add_job(notification, trigger='date', next_run_time=str(date_time))
+    return "job details: %s" % job
+
+
+def notification():
+	with app.app_context():
+	    msg = Message(subject="Hello",
+		              sender=app.config.get("MAIL_USERNAME"),
+		              recipients=["rahulkudva98@gmail.com"], # replace with your email for testing
+		              body="This is a test email I sent with Gmail and Python!")
+	    mail.send(msg)
+
+if __name__ == '__main__':
+	
+	app.run(debug=True,port=2000)
